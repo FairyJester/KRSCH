5782305496a3c5558d2ad9ad3d8a96395e78eed4
FairyJester <98344500+FairyJester@users.noreply.github.com>
2022-11-08 20:10:45 +0300

    Add files via upload

---
+++ Krsch/Krsch/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krsch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int c = int.Parse(textBox1.Text);
+                String[] sNums = textBox2.Text.Split(' ');
+
+                int[] nums = new int[c];
+                for (int i = 0; i < c; i++)
+                {
+                    nums[i] = int.Parse(sNums[i]);
+
+                }
+                
+
+                Form2 form2 = new Form2(nums);
+                form2.Show();
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!!!");
+            }
+            }
+        
+
+      
+    }
+}
---
+++ Krsch/Krsch/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krsch
+{
+    public partial class Form2 : Form
+    {
+        private int[] nums;
+        public Form2(int[] nums)
+        {
+            InitializeComponent();
+            this.nums = nums;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sort(nums);
+
+            String str = "";
+            for (int i = 0; i < nums.Length; i++)
+            {
+                str += nums[i] + " ";
+            }
+            textBox1.Text = str;
+
+
+        }
+        public static void sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int x = array[i];
+
+
+                int j = Math.Abs(Array.BinarySearch(array, 0, i, x) + 1);
+
+
+                System.Array.Copy(array, j, array, j + 1, i - j);
+
+
+                array[j] = x;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
